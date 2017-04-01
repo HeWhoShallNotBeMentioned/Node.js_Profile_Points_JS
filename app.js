03d49d30ca60afa0ec78c0051778d77e4240f3ef
Chris Underwood <cunderwoodmn@gmail.com>
2017-03-31 23:13:08 -0700

    concatenate body text into a string

--- app.js
+++ app.js
@@ -13,6 +13,16 @@ function printMessage(username, badgeCount, points) {
 const request = https.get(`https://teamtreehouse.com/${username}.json`, (response) => {
 console.log(response.statusCode);
   // Read the data
-  // Parse the data
-  // Print the data
+  let body = "";
+  response.on('data', (data) => {
+    body += data.toString();
+  });
+
+  response.on('end', () => {
+    // Parse the data
+    console.log(body);
+    console.log(typeof body);
+    // Print the data
+  });
+
 });
