502cbbfdb3ee075cbd0b3da66c5eecafe88a10cb
Chris Underwood <cunderwoodmn@gmail.com>
2017-04-01 00:21:14 -0700

    wrap main code in getProfile function, wrote a small function to process an array of usernames, and add ability to get usernames from the console

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // Problem: We need a simple way to look at a user's badge count and JavaScript points
 // Solution: Use Node.js to connect to Treehouse's API to get profile information to print out
 const https = require('https');
-const username = 'chrisunderwood';
+
 
 //Function to print message to console
 function printMessage(username, badgeCount, points) {
@@ -9,21 +9,29 @@ function printMessage(username, badgeCount, points) {
   console.log(message);
 }
 
-//connect to API URL(https://teamtreehouse.com/username.json)
-const request = https.get(`https://teamtreehouse.com/${username}.json`, (response) => {
-console.log(response.statusCode);
-  // Read the data
-  let body = "";
-  response.on('data', (data) => {
-    body += data.toString();
+function getProfile(username) {
+  //connect to API URL(https://teamtreehouse.com/username.json)
+  const request = https.get(`https://teamtreehouse.com/${username}.json`, (response) => {
+  console.log(response.statusCode);
+    // Read the data
+    let body = "";
+    response.on('data', (data) => {
+      body += data.toString();
+    });
+
+    response.on('end', () => {
+      // Parse the data
+      const profile = JSON.parse(body);
+      // Print the data
+      printMessage(username, profile.badges.length, profile.points.JavaScript);
+    });
   });
+}
 
-  response.on('end', () => {
-    // Parse the data
-    const profile = JSON.parse(body);
-    // Print the data
-    printMessage(username, profile.badges.length, profile.points.JavaScript);
 
+
+const users = process.argv.slice(2);
+  users.forEach(username => {
+    getProfile(username);
   });
-
-});
+//could also just be users.forEach(getProfile);
