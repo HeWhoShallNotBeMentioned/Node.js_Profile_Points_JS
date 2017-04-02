9bb7428472ae56f6cd64a059c2cdb9113a2d96bc
Chris Underwood <cunderwoodmn@gmail.com>
2017-04-02 00:28:08 -0700

    add error catching for if a 200 status code is received or not and gave a more custom error message that will give the http description of the error code.

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // Problem: We need a simple way to look at a user's badge count and JavaScript points
 // Solution: Use Node.js to connect to Treehouse's API to get profile information to print out
 const https = require('https');
+const http = require('http');
 
 //Print Error Messages
 function printError(error){
@@ -18,24 +19,29 @@ function getProfile(username) {
   try {
     //connect to API URL(https://teamtreehouse.com/username.json)
     const request = https.get(`https://teamtreehouse.com/${username}.json`, (response) => {
-    console.log(response.statusCode);
-      // Read the data
-      let body = "";
-      response.on('data', (data) => {
-        body += data.toString();
-      });
+      if (response.statusCode === 200) {
+        // Read the data
+        let body = "";
+        response.on('data', (data) => {
+          body += data.toString();
+        });
 
-      response.on('end', () => {
-        try{
-          // Parse the data
-          const profile = JSON.parse(body);
-          // Print the data
-          printMessage(username, profile.badges.length, profile.points.JavaScript);
-        } catch(error){
-          printError(error);
-        }
-      });
-    }).on('error', printError(error));
+        response.on('end', () => {
+          try{
+            // Parse the data
+            const profile = JSON.parse(body);
+            // Print the data
+            printMessage(username, profile.badges.length, profile.points.JavaScript);
+          } catch(error){
+            printError(error);
+          }
+        });
+      } else {
+          const message =`There was an error getting the profile for ${username} (${http.STATUS_CODES[response.statusCode]})`;
+          const statusCodeError = new Error(message);
+          printError(statusCodeError);
+      }
+    }).on('error', printError);
   } catch (error) {
     printError(error);
   }
